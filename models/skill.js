3402237a204eb67995cbbfd6818433e54aec8153
Anita9948 <abaljit92@gmail.com>
2022-09-30 19:29:36 -0400

    add skill function

--- models/skill.js
+++ models/skill.js
@@ -1,18 +1,25 @@
 const skills = [
-  { id: 01234, skill: "JavaScript", leveofExpertise: "Junior" },
-  { id: 01235, skill: "Database", leveofExpertise: "Junior" },
-  { id: 01236, skill: "Framework - Express", leveofExpertise: "Junior" },
-  { id: 01237, skill: "Debugging", leveofExpertise: "Junior" },
+  { id: 1234, skill: "JavaScript", leveofExpertise: "Junior" },
+  { id: 1235, skill: "Database", leveofExpertise: "Junior" },
+  { id: 1236, skill: "Framework - Express", leveofExpertise: "Junior" },
+  { id: 1237, skill: "Debugging", leveofExpertise: "Junior" },
 ];
 
 module.exports = {
   getAll,
-  getOne
+  getOne,
+  create
 };
 function getAll() {
   return skills;
 }
 
 function getOne(id) {
-  return skills.find((s) => s.id === id);
+  console.log(id);
+  return getAll().find((s) => s.id === id);
+}
+
+function create(skill) {
+  skill.id = Date.now();
+  skills.push(skill);
 }
