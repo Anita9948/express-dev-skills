7f74ba1e5b9a78ce8af1e762c05bd6fcfc1208f3
Anita9948 <abaljit92@gmail.com>
2022-09-30 19:28:49 -0400

    add skill

--- controller/skills.js
+++ controller/skills.js
@@ -1,23 +1,30 @@
-const Skill = require('../models/skill')
+const Skill = require("../models/skill");
 
 module.exports = {
   index,
   new: newSkill,
-  show
+  show,
+  create,
 };
 
 function index(req, res) {
-  res.render("skills/index", {
-    title: "My Skills",
-  });
+  let skills = Skill.getAll();
+  res.render("skills/index", { title: "My Skills", skills });
 }
 
 function newSkill(req, res) {
-  res.render("skills/show", {
-    skill: Skill.getOne(req.params.id),
-  });
+  res.render("skills/new", { title: "My Skills" });
 }
-function show(req,res) {
-  let skill= Skill.getOne(parseInt(req.params.id))
-  res.render('skills/show',{title: "Showing Skill", skill})
+
+function show(req, res) {
+  console.log(req.params.id);
+  let skill = Skill.getOne(parseInt(req.params.id));
+  console.log(skill);
+  res.render("skills/show", { title: "Showing Skill", skill });
+}
+
+function create(req, res) {
+  console.log(req.body)
+  // Skill.push(skill);
+  res.redirect("/skills/new");
 }
