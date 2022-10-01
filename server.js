0e7602f6e99468598860c27c02015e64545e21cc
Anita9948 <abaljit92@gmail.com>
2022-09-30 21:28:41 -0400

    fix method-override

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-const methodOverride = require('method-override')
+const methodOverride = require("method-override");
 
 var skillsRouter = require("./routes/skills");
 
@@ -18,10 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(methodOverride('_method'));
-app.get('/',function (req,res) {
-  res.render('index',{title: ""})
-})
+app.use(methodOverride("_method"));
+app.get("/", function (req, res) {
+  res.render("index", { title: "" });
+});
 app.use("/skills", skillsRouter);
 
 // catch 404 and forward to error handler
